82d8292e21056ae303322e147579cd8561648f87
Diane Trout <diane@ghic.org>
2017-05-02 17:00:12 -0700

    Assume that a combined quantificatio file is of a single tube type
    Manually listing off every library id was really annoying.
    
    This split loading the combned and rsem file types into seperate
    functions, and refactored the calculation into its own function.

--- gene_spike_ratio.py
+++ gene_spike_ratio.py
@@ -6,7 +6,7 @@ import logging
 import pandas
 from scipy.stats import ttest_ind
 
-from singleqc import configure_logging, read_concentrations
+from singleqc import configure_logging
 
 logger = logging.getLogger('gene_spike_ratio')
 
@@ -18,56 +18,48 @@ def main(cmdline=None):
     if len(args.rsem) != len(args.rsem_library):
         parser.error("every rsem filename must be paired with a library ID")
 
-    logger.debug('Filenames: %s', ','.join(args.filenames))
-    logger.debug('RSEM: %s', ','.join(args.rsem))
-    logger.debug('Poolids: %s', ','.join(args.pool))
+    data = []
+    data.extend(read_rsem_quantifications(args.rsem, args.rsem_library, args.pool, args.single, args.quantification_name))
+    data.extend(read_combined_quantification(args.combined_pool, 'pool', args.sep))
+    data.extend(read_combined_quantification(args.combined_single, 'single', args.sep))
+    data = pandas.DataFrame(data)
+    print(data)
 
-    concentrations = read_concentrations(args.concentrations)
-    data = read_quantifications(args.filenames, args.rsem, args.rsem_library, args.rsem_quantification, args.sep)
+    pool = data[data['tube_type'] == 'pool']['ratio']
+    if len(pool) > 0:
+        print('Pool-split: mean {:.3} stdev {:.3}'.format(pool.mean(), pool.std()))
 
-    spikes = ['Spikein' in x for x in data.index ]
-    notspikes = ['Spikein' not in x for x in data.index ]
+    single = data[data['tube_type'] == 'single']['ratio']
+    if len(single) > 0:
+        print('Single: mean {:.3} stdev {:.3}'.format(single.mean(), single.std()))
 
-    spike_sum = data[spikes].sum(axis=0)
-    spike_sum.name = 'spike_sum'
-    gene_sum = data[notspikes].sum(axis=0)
-    gene_sum.name = 'gene_sum'
-    ratio = (spike_sum/(spike_sum+gene_sum))*100
-    ratio.name = 'ratio'
-    names = make_tube_type_table(args, ratio.index)
-
-    df = pandas.DataFrame([gene_sum, spike_sum, ratio]).T
-    df = df.merge(names, left_index=True, right_index=True)
-    print(df)
-
-    if len(args.pool) > 0:
-        pool = ratio[args.pool]
-        print('pool mean', pool.mean())
-
-    if len(args.single) > 0:
-        single = ratio[args.single]
-        print('single mean', single.mean())
-
-    if len(args.pool) > 0 and len(args.single) > 0:
+    if len(pool) > 0 and len(single) > 0:
         t = ttest_ind(pool, single, equal_var=False)
-        
         print(t)
 
 
 def make_parser():
     parser = argparse.ArgumentParser()
-    parser.add_argument('filenames', nargs='*', help='merged quantification file to read')
-    parser.add_argument('--rsem', action='append', default=[],
-                        help='name of rsem quantification files ')
-    parser.add_argument('--rsem-library', action='append', default=[],
-                        help='library id for rsem quantification file')
-    parser.add_argument('--pool', action='append', default=[],
-                        help='pool-split library names')
-    parser.add_argument('--single', action='append', default=[],
-                        help='single-cell library names (default)')
+
+    group = parser.add_argument_group('combined quantification file')
+    group.add_argument('--combined-pool', action='append', default=[],
+                        help='file with merged pool-split quantifications to read')
+    group.add_argument('--combined-single', action='append', default=[],
+                        help='file with merge single cell quantifications to read')
+
+    group = parser.add_argument_group('raw RSEM files')
+    group.add_argument('--rsem', action='append', default=[],
+                       help='name of rsem quantification files ')
+    group.add_argument('--rsem-library', action='append', default=[],
+                       help='library id for rsem quantification file')
+    group.add_argument('--pool', action='append', default=[],
+                       help='pool-split library names')
+    group.add_argument('--single', action='append', default=[],
+                       help='single-cell library names (default)')
+
     parser.add_argument('-c', '--concentrations', required=True,
                         help='name of file with concentrations for spike ins')
-    parser.add_argument('-q', '--rsem-quantification', default='FPKM',
+    parser.add_argument('-q', '--quantification-name', default='FPKM',
                         help='Which RSEM quantification column to use')
     parser.add_argument('-o', '--output', help='output name')
     parser.add_argument('-v', '--verbose', action='store_true')
@@ -76,32 +68,51 @@ def make_parser():
 
     return parser
 
-def read_quantifications(filenames, rsem, rsem_ids, quantification, sep):
+
+def compute_gene_spike_ratios(library_id, quantification, tube_type):
+    '''Compare the sum of genes and sum of spiikes.
+    '''
+    spikes = ['Spikein' in x for x in quantification.index ]
+    notspikes = ['Spikein' not in x for x in quantification.index ]
+
+    spike_sum = quantification[spikes].sum(axis=0)
+    gene_sum = quantification[notspikes].sum(axis=0)
+    ratio = (spike_sum/(spike_sum+gene_sum))*100
+
+    s = pandas.Series([gene_sum, spike_sum, ratio, tube_type],
+                      index=['gene_sum', 'spike_sum', 'ratio', 'tube_type'])
+    s.name = library_id
+    return s
+
+
+def read_combined_quantification(filenames, tube_type, sep):
     data = []
     for filename in filenames:
-        data.append(pandas.read_csv(filename, sep=sep, index_col=0))
+        combined = pandas.read_csv(filename, sep=sep, index_col=0)
+        for column in combined.columns:
+            if column == 'gene_name':
+                logger.warning('Ignoring column named gene_name')
+            else:
+                data.append(compute_gene_spike_ratios(column, combined[column], tube_type))
+    return data
 
-    for filename, library_id in zip(rsem, rsem_ids):
-        current = pandas.read_csv(filename, sep='\t',
-                                  index_col=0)
-        current = current[[quantification]]
-        current.columns = [library_id]
-        current.index.name = 'gene_id'
-        data.append(current)
 
-    return pandas.concat(data, axis=1)
-
-def make_tube_type_table(args, runs):
-    names = []
-    for run_name in runs:
-        if run_name in args.pool:
-            names.append('pool')
-        elif run_name in args.single:
-            names.append('single')
+def read_rsem_quantifications(rsem, library_ids, pool_ids, single_ids, quantification_name):
+    data = []
+    for filename, library_id in zip(rsem, library_ids):
+        current = pandas.read_csv(filename, sep='\t', index_col=0)
+        current = current[quantification_name]
+        if library_id in pool_ids:
+            tube_type = 'pool'
+        elif library_id in single_ids:
+            tube_type = 'single'
         else:
-            names.append('NOT DEFINED')
+            tube_type = 'NOT DEFINED'
 
-    return pandas.DataFrame(names, columns=['tube_type'], index=runs)
+        ratios = compute_gene_spike_ratios(library_id, current, tube_type)
+        data.append(ratios)
+
+    return data
 
     
 if __name__ == '__main__':
